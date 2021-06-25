8e5f8005f7fe5b1dc2f64f35f037c64181c2f9f6
mattCornicDev <glgharib@gmail.com>
2021-06-25 10:06:58 +0200

    Init

---
+++ ExerciceSynthese/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ExerciceSynthese.Dal;
+using ExerciceSynthese.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExerciceSynthese
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
